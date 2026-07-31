73c8d657967132cd66020f52be537495f259778c
krish6925 <krishbhalotia9876@gmail.com>
2026-07-31 23:29:37 +0530

    Fix casing in middleware import paths
    Corrected casing in import statements for middleware.

--- backend/routes/departmentRoutes.js
+++ backend/routes/departmentRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 
-import protect from "../middleware/authMiddleware.js";
-import authorize from "../middleware/roleMiddleware.js";
+import protect from "../middleware/authmiddleware.js";
+import authorize from "../middleware/rolemiddleware.js";
 
 import {
     createDepartment,
